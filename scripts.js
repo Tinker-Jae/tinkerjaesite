009a380fe4dc13050d4013704763f2b9cf01dca8
tinker-jae <dstorm6398@gmail.com>
2023-04-18 16:50:44 -0400

    added navbars to js so it changes across the "entire site" now

--- scripts.js
+++ scripts.js
@@ -1,7 +1,37 @@
-var NavbarHTML = ' <a href="/">Home</a>     <a href="/Pages/All Posts/!Posts.html">Posts</a>      <a href="/Pages/Misc/!misc.html">Misc</a> '
 
+// This controls the Navbar for Mobile Screens
+var NavbarMobileHTML = '<h1><a href="/">Home</a></h1> <h1><a href="/Pages/All Posts/!Posts.html">Posts</a></h1> <h1><a href="/Pages/Misc/!misc.html">Misc</a></h1>'
+// Here's the code for NavbarMobile
+/* 
+<h1><a href="/">Home</a></h1>
+<h1><a href="/Pages/All Posts/!Posts.html">Posts</a></h1>
+<h1><a href="/Pages/Misc/!misc.html">Misc</a></h1>
+*/
+
+
+// This controls the Navbar for Desktop Screens
+var NavbarDesktopHTML = '<h1><a href="/">Home</a></h1><hr> <h1><a href="/Pages/All Posts/!Posts.html">Posts</a></h1><hr> <h1><a href="/Pages/Misc/!misc.html">Misc</a></h1>'
+// Here's the code for NavbarDesktop
+/*
+<h1><a href="/">Home</a></h1>
+<hr>
+<h1><a href="/Pages/All Posts/!Posts.html">Posts</a></h1>
+<hr>
+<h1><a href="/Pages/Misc/!misc.html">Misc</a></h1>
+*/
+
+
+// this line controls the site title
 var SiteTitleHTML = 'Tinker Jae'
 
+// This links the site title on your page to the SiteTitle Variable
 document.getElementById("SiteTitle").innerHTML = SiteTitleHTML;
 
-document.getElementById("Navbar").innerHTML = NavbarHTML; 
+// This links the Navbar on your mobile page to the NavbarMobile Variable
+document.getElementById("NavbarDesktop").innerHTML = NavbarMobileHTML; 
+
+// This links the Navbar on your desktop page to the NavbarDesktop Variable
+document.getElementById("NavbarDesktop").innerHTML = NavbarDesktopHTML; 
+
+
+
