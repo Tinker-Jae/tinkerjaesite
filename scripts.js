eb50418ec22b6c03231523ae3633338ed86a741b
tinker-jae <dstorm6398@gmail.com>
2023-05-11 16:37:28 -0400

    Version 1.05 complete
    Changelog v1.05
    - Changed layout a whole bunch.
    - Added categories for stuff
    - Added new Post system, works via JS.
    - Welcome, Sitemap, Abt me, Links, and blinkies added to homepage.
    - Updated posts to new format
    - turned left and right box into JS containers?
    - Right box gone.
    - Mobile friendly I guess?
    
    Big ass site update but yeah, posts are a lot easier, formatting is better OH BEFORE I FORGET
    
    - added debug container to EVERY page
    - If debug container is not on every page it may break some javascript features.
    
    IE: if you have a thing to put a picture on the screen with JS, if that code is not called upon, it breaks EVERYTHING, after it in JS.
    Posts, images, innerhtml, nothing will replace anything else unless it's being called by Debug.
    
    DO NOT REMOVE DEBUG FROM ANYTHING IT WILL BREAK THE ENTIRE SITE.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,37 @@
+let debugHTML = '<div class="debug" id="RecentPosts"></div><div class="debug" id="AllPosts"></div><div class="debug" id="JournalPosts"></div><div class="debug" id="ArticlePosts"></div><div class="debug" id="ProjectPosts"></div><div class="debug" id="WritingPosts"></div><div class="debug" id="MiscPosts"></div><div class="debug" id="Marquee"></div><div class="debug" id="SiteTitle"></div><div class="debug" id="NavbarMobile"></div> <div class="debug" id="NavbarDesktop"></div> <div class="debug" id="RightBox"></div> <div class="debug" id="LeftBox"></div> <div class="debug" id="ProfPic"></div>';
+/*
+document.getElementById("LeftBox").innerHTML = LeftBoxHTML;
+// LINKS TO ANY ITEM WITH A PROF PIC ID TAG ON IT
+document.getElementById("ProfPic").src = ProfPicSRC;
+
+
+*/
+
+document.getElementById("debug2").innerHTML = debugHTML;
+
 // This controls the Navbar for Mobile Screens
-var NavbarMobileHTML = '<h3><a href="/">Home</a></h3> <h3><a href="/Pages/All Posts/!Posts.html">Shrines</a></h3>  <h3><a href="/Pages/All Posts/!Posts.html">Blog</a></h3> <h3><a href="/Pages/Misc/!misc.html">Misc</a></h3>';
+var NavbarMobileHTML = '<h3><a href="/">Home</a></h3> <h3><a href="/Pages/!Projects.html">Projects</a></h3> <h3><a href="/Pages/!Articles.html">Articles</a></h3> <h3><a href="/Pages/!Journal.html">Journal</a></h3> <h3><a href="/Pages/!Writings.html">Writings</a></h3> <h3><a href="/Pages/!Misc.html">Misc</a></h3>';
 // Here's the code for NavbarMobile
 /* 
 <h3><a href="/">Home</a></h3>
-<h3><a href="/Pages/All Posts/!Posts.html">Shrines</a></h3> 
-<h3><a href="/Pages/All Posts/!Posts.html">Blog</a></h3>
-<h3><a href="/Pages/Misc/!misc.html">Misc</a></h3>
+<h3><a href="/Pages/!Projects.html">Projects</a></h3>
+<h3><a href="/Pages/!Articles.html">Articles</a></h3>
+<h3><a href="/Pages/!Journal.html">Journal</a></h3>
+<h3><a href="/Pages/!Writings.html">Writings</a></h3>
+<h3><a href="/Pages/!Misc.html">Misc</a></h3>
 */
 
 
 // This controls the Navbar for Desktop Screens
-var NavbarDesktopHTML = '<h3><a href="/">Home</a></h3><hr> <h3><a href="/Pages/All Posts/!Posts.html">Shrines</a></h3><hr> <h3><a href="/Pages/All Posts/!Posts.html">Blog</a></h3><hr> <h3><a href="/Pages/Misc/!misc.html">Misc</a></h3>';
+var NavbarDesktopHTML = '<h3><a href="/">Home</a></h3><hr> <h3><a href="/Pages/!Projects.html">Projects</a></h3><hr> <h3><a href="/Pages/!Articles.html">Articles</a></h3><hr> <h3><a href="/Pages/!Journal.html">Journal</a></h3><hr> <h3><a href="/Pages/!Writings.html">Writings</a></h3><hr> <h3><a href="/Pages/!Misc.html">Misc</a></h3>';
 // Here's the code for NavbarDesktop
 /*
-<h3><a href="/">Home</a></h3><hr> 
-<h3><a href="/Pages/All Posts/!Posts.html">Shrines</a></h3><hr> 
-<h3><a href="/Pages/All Posts/!Posts.html">Blog</a></h3><hr>
-<h3><a href="/Pages/Misc/!misc.html">Misc</a></h3>
+<h3><a href="/">Home</a></h3><hr>
+<h3><a href="/Pages/!Projects.html">Projects</a></h3><hr>
+<h3><a href="/Pages/!Articles.html">Articles</a></h3><hr>
+<h3><a href="/Pages/!Journal.html">Journal</a></h3><hr>
+<h3><a href="/Pages/!Writings.html">Writings</a></h3><hr>
+<h3><a href="/Pages/!Misc.html">Misc</a></h3>
 */
 
 // Code for the Blinky Marquee
@@ -40,6 +55,15 @@ var MarqueeHTML = '<div><img src="/Attachments/Blinkies/0142-virusgender.gif"></
 <div><img src="https://files.catbox.moe/hwsop4.gif"></div>  
 */
 
+var ProfPicSRC = '/Attachments/profpic.png';
+
+// Code for the LeftBox since it's the same on all pages.
+var LeftBoxHTML = '<img class="ProfPic Border" id="ProfPic"> <div class="NavbarDesktop Border" id="NavbarDesktop"></div>';
+/*
+<img class="ProfPic Border" src="/Attachments/profpic.png">
+<div class="NavbarDesktop Border" id="NavbarDesktop"></div>
+*/
+
 // Code for the RightBox since it's the same on all pages.
 var RightBoxHTML = '<h1><u>Pic of the Day</u></h1> <div class="PostImageBlock"><a href="https://twitter.com/hitsujigoods/status/1605248664158486536?s=20"><img src="https://files.catbox.moe/3q7hvi.jpg"></a></div> <h1><u>Recent Posts</u></h1> <div class="RecentPostThing" id="RecentPosts"></div>';
 /*
@@ -55,6 +79,10 @@ var RightBoxHTML = '<h1><u>Pic of the Day</u></h1> <div class="PostImageBlock"><
 var SiteTitleHTML = '<a href="/" class="SiteTitleLink">Tinker Jae</a>';
 // This links the site title on your page to the SiteTitle Variable
 document.getElementById("SiteTitle").innerHTML = SiteTitleHTML;
+// Links to the Leftbox on page, with the LeftBoxHTML Variable
+document.getElementById("LeftBox").innerHTML = LeftBoxHTML;
+// LINKS TO ANY ITEM WITH A PROF PIC ID TAG ON IT
+document.getElementById("ProfPic").src = ProfPicSRC;
 // This links the Navbar on your mobile page to the NavbarMobile Variable
 document.getElementById("NavbarMobile").innerHTML = NavbarMobileHTML;
 // This links the Navbar on your desktop page to the NavbarDesktop Variable
@@ -65,18 +93,22 @@ document.getElementById("Marquee").innerHTML = MarqueeHTML;
 document.getElementById("RightBox").innerHTML = RightBoxHTML;
 
 
+
+
 //THIS IS ALL FOR THE POSTS ARRAY
 //THIS IS ALL FOR THE POSTS ARRAY
 //THIS IS ALL FOR THE POSTS ARRAY
 
 
 // This holds every single post we will ever make
-// Each Category, All, Blog, Shrine, Project, Library, and Misc define the category of the post and have a subsequent array and posts variable
+// Each Category, All, Journal, Article, Project, Writing, and Misc define the category of the post and have a subsequent array and posts variable
 //When adding new posts, change the stuff, and add the newest at the bottom.
 var postsArray = [
-    {Link:"https://www.google.com/", Title:"Google", Description:"It's an internet search engine.", Date:"09/04/1998", All:true, Blog:false, Shrine:false, Project:false, Library:false, Misc:false},
-    {Link:"https://www.youtube.com/", Title:"Youtube", Description:"It's a video hosting platform.", Date:"02/14/2005", All:true, Blog:false, Shrine:false, Project:false, Library:false, Misc:false},
-    {Link:"https://neocities.org/", Title:"Neocities", Description:"It's a social media/web hosting platform.", Date:"05/24/2013",  All:true, Blog:false, Shrine:false, Project:false, Library:false, Misc:false}
+    {Link:"/Pages/Archive/Blog4-2-23.html", Title:"Blog Post", Description:"A blog post.", Date:"4/2/23",  All:true, Project:false, Article:false, Journal:true, Writing:false, Misc:false},
+    {Link:"/Pages/Archive/V1.03Changelog.html", Title:"V1.03 Changelog", Description:"Changelog for v1.03", Date:"idk",  All:true, Project:false, Article:false, Journal:false, Writing:false, Misc:true},
+    {Link:"/Pages/Archive/V1.04Changelog.html", Title:"V1.04 Changelog", Description:"Changelog for v1.04", Date:"Idk",  All:true, Project:false, Article:false, Journal:false, Writing:false, Misc:true},
+    {Link:"/Pages/Archive/CategoryTemplate.html", Title:"Category Template", Description:"Template for Categories.", Date:"5/11/23",  All:true, Project:false, Article:false, Journal:false, Writing:false, Misc:true},
+    {Link:"/Pages/Archive/PostTemplate.html", Title:"Post Template", Description:"Template for Posts.", Date:"5/11/23",  All:true, Project:false, Article:false, Journal:false, Writing:false, Misc:true},
 
     // ADD NEW POST HERE^^^^^^^^^^^^^
 ];
@@ -84,7 +116,7 @@ var postsArray = [
 // this adds tags to make our post readable.
 // it takes the data from postsArray, and adds it to the post template that appears on each page.
 function formattingFunction(a){
-    return ("- <a href='"+a.Link+"'>"+a.Title+"</a> <br> &nbsp;&nbsp;"+a.Description+"<br> &nbsp;&nbsp;"+a.Date+"<br>");
+    return ("<div><a href='"+a.Link+"'>"+a.Title+"</a> ("+a.Date+")<br> "+a.Description+"<br></div>");
 }
 
 // this adds tags for recent posts, h4 being the recent post tag.
@@ -100,16 +132,16 @@ for (let i=0; i<postsArray.length; i++) {
         allArray = [...allArray, postsArray[i]];
     }
 }
-let blogArray = [];
+let journalArray = [];
 for (let i=0; i<postsArray.length; i++) {
-    if (postsArray[i].Blog === true) {
-        blogArray = [...blogArray, postsArray[i]];
+    if (postsArray[i].Journal === true) {
+        journalArray = [...journalArray, postsArray[i]];
     }
 }
-let shrineArray = [];
+let articleArray = [];
 for (let i=0; i<postsArray.length; i++) {
-    if (postsArray[i].Shrine === true) {
-        shrineArray = [...shrineArray, postsArray[i]];
+    if (postsArray[i].Article === true) {
+        articleArray = [...articleArray, postsArray[i]];
     }
 }
 let projectArray = [];
@@ -118,10 +150,10 @@ for (let i=0; i<postsArray.length; i++) {
         projectArray = [...projectArray, postsArray[i]];
     }
 }
-let libraryArray = [];
+let writingArray = [];
 for (let i=0; i<postsArray.length; i++) {
-    if (postsArray[i].Library === true) {
-        libraryArray = [...libraryArray, postsArray[i]];
+    if (postsArray[i].Writing === true) {
+        writingArray = [...writingArray, postsArray[i]];
     }
 }
 let miscArray = []; 
@@ -139,10 +171,10 @@ for (let i=0; i<postsArray.length; i++) {
 
 // this makes it so it goes from most recent (bottom) to least recent (top)
 allArray.reverse();
-blogArray.reverse();
-shrineArray.reverse();
+journalArray.reverse();
+articleArray.reverse();
 projectArray.reverse();
-libraryArray.reverse();
+writingArray.reverse();
 miscArray.reverse();
 recentArray.reverse();
 
@@ -152,21 +184,21 @@ let allPosts = "";
 for (let i=0; i < allArray.length; i++){
     allPosts += formattingFunction(allArray[i]);
 }
-let blogPosts = "";
-for (let i=0; i < blogArray.length; i++){
-    blogPosts += formattingFunction(blogArray[i]);
+let journalPosts = "";
+for (let i=0; i < journalArray.length; i++){
+    journalPosts += formattingFunction(journalArray[i]);
 }
-let shrinePosts = "";
-for (let i=0; i < shrineArray.length; i++){
-    shrinePosts += formattingFunction(shrineArray[i]);
+let articlePosts = "";
+for (let i=0; i < articleArray.length; i++){
+    articlePosts += formattingFunction(articleArray[i]);
 }
 let projectPosts = "";
 for (let i=0; i < projectArray.length; i++){
     projectPosts += formattingFunction(projectArray[i]);
 }
-let libraryPosts = "";
-for (let i=0; i < libraryArray.length; i++){
-    libraryPosts += formattingFunction(libraryArray[i]);
+let writingPosts = "";
+for (let i=0; i < writingArray.length; i++){
+    writingPosts += formattingFunction(writingArray[i]);
 }
 let miscPosts = "";
 for (let i=0; i < miscArray.length; i++){
@@ -183,34 +215,12 @@ for (let i=0; i < recentPostsCutoff; i++){
 // Each of these have their own debug div on EACH page.
 // for some reason if one of the variables isn't called, then any of the variables after that will not work. Like at all
 document.getElementById("AllPosts").innerHTML = allPosts;
-document.getElementById("BlogPosts").innerHTML = blogPosts;
-document.getElementById("ShrinePosts").innerHTML = shrinePosts;
+document.getElementById("JournalPosts").innerHTML = journalPosts;
+document.getElementById("ArticlePosts").innerHTML = articlePosts;
 document.getElementById("ProjectPosts").innerHTML = projectPosts;
-document.getElementById("LibraryPosts").innerHTML = libraryPosts;
+document.getElementById("WritingPosts").innerHTML = writingPosts;
 document.getElementById("MiscPosts").innerHTML = miscPosts;
 document.getElementById("RecentPosts").innerHTML = recentPosts;
-
-// Put this in every page.
-/* 
-<!--DO NOT REMOVE DEBUG IT WILL BREAK ALL JAVASCRIPT-->
-<div class="debug">
-<!--DO NOT REMOVE DEBUG IT WILL BREAK ALL JAVASCRIPT-->
-<div class="debug" id="RecentPosts"></div>
-<div class="debug" id="AllPosts"></div>
-<div class="debug" id="BlogPosts"></div>
-<div class="debug" id="ShrinePosts"></div>
-<div class="debug" id="ProjectPosts"></div>
-<div class="debug" id="LibraryPosts"></div>
-<div class="debug" id="MiscPosts"></div>
-<div class="debug" id="Marquee"></div>
-<div class="debug" id="SiteTitle"></div>
-<div class="debug" id="NavbarMobile"></div>
-<div class="debug" id="NavbarDesktop"></div>
-<!--DO NOT REMOVE DEBUG IT WILL BREAK ALL JAVASCRIPT-->
-</div>
-<!--DO NOT REMOVE DEBUG IT WILL BREAK ALL JAVASCRIPT-->
-
-*/
 
 //THIS IS ALL FOR THE POSTS ARRAY
 //THIS IS ALL FOR THE POSTS ARRAY
